e9687134cbed357c45c3b09e277ae021f071cb2b
Johnstanley Ajagu <124546871+Johnstanley1@users.noreply.github.com>
2024-02-01 22:53:24 -0500

    NUnit Test class library added for testing the rectangle class methods

---
+++ JANCAssignment2Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace JANCAssignment2Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
